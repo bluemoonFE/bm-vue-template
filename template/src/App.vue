0df4b0f1f4cc486176da9ea3ebf3375244f3a4a3
russellmars <russellmars@sina.com>
2018-12-05 17:27:47 +0800

    优化

--- template/src/App.vue
+++ template/src/App.vue
@@ -12,10 +12,13 @@
 </style>
 
 <script>
+{{#unless router}}
 import HelloWorld from './components/HelloWorld'
+{{/unless}}
 export default {
+  name: 'App'{{#router}}{{else}},
   components: {
     HelloWorld
-  }
+  }{{/router}}
 }
 </script>
